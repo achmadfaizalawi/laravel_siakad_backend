5d757373c14e71b475d727706997c49738fa393c
Achmad Faiz Alawi <achmad.alawi@gmail.com>
2023-11-12 07:26:26 +0700

    Auth and home page ready

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; Achmad Faiz Alawi 2023
+  </div>
---
+++ resources/views/components/error-footer.blade.php
@@ -0,0 +1,3 @@
+<div class="simple-footer mt-5">
+    Copyright &copy; Achmad Faiz Alawi 2023
+</div>
